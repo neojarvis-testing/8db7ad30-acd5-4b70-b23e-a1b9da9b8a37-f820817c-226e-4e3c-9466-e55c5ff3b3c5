f4a454577f6eb1dfeaf81810ecfcd4913e04c761
balajibabuVirtusa <balajibabu@virtusa.com>
2025-05-22 20:37:24 +0000

    AppDbContext, Models classes

---
+++ CommonLibrary/Models/UserRoles.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CommonLibrary.Models
+{
+    public class UserRoles
+    {
+        public const string Admin;
+        public const string User;
+    }
+}
