8e4f99b0cb0ed6189e688b4912bb20bdf4bff20a
jafrinfrancis <jafrinfrancis@gmail.com>
2025-05-23 11:41:58 +0530

    FeedbackControllerChanges

--- CommonLibrary/Models/Feedback.cs
+++ CommonLibrary/Models/Feedback.cs
@@ -1,16 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
+﻿
 namespace CommonLibrary.Models
 {
     public class Feedback
     {
         public int FeedbackId { get; set; }
         public int UserId { get; set; }
-        public User? User { get; set; }
-        public string FeedbackText { get; set; }
+        //public User? User { get; set; }
+        public string? FeedbackText { get; set; }
         public DateTime Date { get; set; }
+
     }
 }
---
+++ dotnetapp4/Services/IFeedbackService.cs
@@ -0,0 +1,12 @@
+﻿using CommonLibrary.Models;
+
+namespace dotnetapp4.Services
+{
+    public interface IFeedbackService
+    {
+        Task<bool> AddFeeback(Feedback feedback);
+        Task<bool> DeleteFeeback(int feedbackId);
+        Task<IEnumerable<Feedback>> GetAllFeedbacks();
+        Task<IEnumerable<Feedback>> GetFeedbacksByUserId(int userId);
+    }
+}
