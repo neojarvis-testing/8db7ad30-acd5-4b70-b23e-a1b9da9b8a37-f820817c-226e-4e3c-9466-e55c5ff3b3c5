0da6ef49048de78847d7b9b33fb2650436259e97
VindyaDhaneshiGK <vindyadgardiyawasam@outlook.com>
2025-05-25 15:17:02 +0000

    conference event components added

---
+++ angularapp/src/app/components/admin-view-conference-event/admin-view-conference-event.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-view-conference-event',
+  templateUrl: './admin-view-conference-event.component.html',
+  styleUrls: ['./admin-view-conference-event.component.css']
+})
+export class AdminViewConferenceEventComponent {
+
+}
