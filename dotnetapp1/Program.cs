89710a89e879750cb9d46a3bfe6f58b078af33a2
9381422989 <gantahemanth75@gmail.com>
2025-05-26 07:07:38 +0000

    corspolicy

--- dotnetapp1/Program.cs
+++ dotnetapp1/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy",
+        builder => builder.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString: builder.Configuration.GetConnectionString("AppCon")));
 builder.Services.AddIdentityCore<ApplicationUser>(options=>options.SignIn.RequireConfirmedAccount=true).AddRoles<IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
@@ -20,6 +27,7 @@ builder.Services.AddScoped<IAuthService,AuthService>();
 
 
 var app = builder.Build();
+app.UseCors("CorsPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
