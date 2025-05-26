30ebba5816a01763ef3bd1e4d5bd444d834bd026
jafrinfrancis <jafrinfrancis@gmail.com>
2025-05-26 19:34:49 +0530

    base url updated

--- angularapp/src/environments/environment.ts
+++ angularapp/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   apiBaseUrl: 'https://8080-dedbbeabbedbaacafdecbcbbfceececffbc.project.examly.io/api'
+  // apiBaseUrl: 'https://8080-cfddbbbdedacfbedbaacafdecbcbbfceececffbc.project.examly.io/api' // base url jafrin
   // apiBaseUrl: 'http://localhost:8080/api' // Uncomment for local development
 };
