4257f7aaf199a13eb4bebb5b3bd453b7016b5478
jafrin <jafrinfrancis@virtusa.com>
2025-05-26 19:21:13 +0530

    Moved base URL to environment.ts

---
+++ angularapp/src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  apiBaseUrl: 'https://8080-dedbbeabbedbaacafdecbcbbfceececffbc.project.examly.io/api'
+  // apiBaseUrl: 'http://localhost:8080/api' // Uncomment for local development
+};
