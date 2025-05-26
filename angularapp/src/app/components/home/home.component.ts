403af9fc245ba5a9fc9bc78d6b01727a5dbe20b6
balajibabuVirtusa <balajibabu@virtusa.com>
2025-05-26 11:29:49 +0530

    home component changes
    home component changes

--- angularapp/src/app/components/home/home.component.ts
+++ angularapp/src/app/components/home/home.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  // You can add necessary logic for the home component here.
 }
