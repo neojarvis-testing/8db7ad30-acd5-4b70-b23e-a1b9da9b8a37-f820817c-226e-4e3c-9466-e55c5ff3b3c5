1395b7d985e7d41a3dc861b0c69f90819281c0df
jafrinfrancis <jafrinfrancis@gmail.com>
2025-05-27 23:13:50 +0530

    loader for all api calls

--- angularapp/src/app/services/auth.interceptor.ts
+++ angularapp/src/app/services/auth.interceptor.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HttpErrorResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, finalize } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { LoaderService } from './loader.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private loaderService: LoaderService) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    this.loaderService.show();
     return next.handle(req).pipe(
       catchError((error: HttpErrorResponse) => {
         if (error.status === 401) {
@@ -16,11 +18,13 @@ export class AuthInterceptor implements HttpInterceptor {
           localStorage.removeItem('role');
           localStorage.removeItem('userId');
           localStorage.removeItem('userName');
-          // Show toast
           this.showToast('Session timed out. Please login again.');
           this.router.navigate(['/login']);
         }
         return throwError(() => error);
+      }),
+      finalize(() => {
+        this.loaderService.hide();
       })
     );
   }
