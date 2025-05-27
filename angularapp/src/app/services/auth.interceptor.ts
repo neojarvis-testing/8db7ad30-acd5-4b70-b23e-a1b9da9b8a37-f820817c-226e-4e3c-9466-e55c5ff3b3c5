c61e76697bdcd27e9c4556cbf657949c232ffa8e
jafrinfrancis <jafrinfrancis@gmail.com>
2025-05-28 00:13:18 +0530

    Seperated toast to a service

--- angularapp/src/app/services/auth.interceptor.ts
+++ angularapp/src/app/services/auth.interceptor.ts
@@ -4,10 +4,15 @@ import { Observable, throwError, finalize } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
 import { LoaderService } from './loader.service';
+import { ToastService } from './toast.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-  constructor(private router: Router, private loaderService: LoaderService) {}
+  constructor(
+    private router: Router,
+    private loaderService: LoaderService,
+    private toastService: ToastService
+  ) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     this.loaderService.show();
@@ -18,7 +23,7 @@ export class AuthInterceptor implements HttpInterceptor {
           localStorage.removeItem('role');
           localStorage.removeItem('userId');
           localStorage.removeItem('userName');
-          this.showToast('Session timed out. Please login again.');
+          this.toastService.show('Session timed out. Please login again.');
           this.router.navigate(['/login']);
         }
         return throwError(() => error);
@@ -27,19 +32,5 @@ export class AuthInterceptor implements HttpInterceptor {
         this.loaderService.hide();
       })
     );
-  }
-
-  private showToast(message: string) {
-    let toast = document.createElement('div');
-    toast.className = 'global-toast';
-    toast.innerText = message;
-    document.body.appendChild(toast);
-    setTimeout(() => {
-      toast.classList.add('show');
-    }, 10);
-    setTimeout(() => {
-      toast.classList.remove('show');
-      setTimeout(() => document.body.removeChild(toast), 400);
-    }, 3000);
   }
 }
